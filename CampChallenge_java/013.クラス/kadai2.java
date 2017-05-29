b350b84fce33ef1e8ba8b91497f80bfa08ca0790
XTEND\XTEND_495 <s1200164@gmail.com>
2017-05-29 15:38:51 +0900

    クラスの継承　クラスの継承の課題です

---
+++ CampChallenge_java/013.クラス/kadai2.java
@@ -0,0 +1,21 @@
+
+import org.camp.servlet.kadai1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author XTEND-495
+ */
+public class kadai2 {
+    class clear extends kadai1{
+        public void reset(){
+            i=0;
+            j=0;            
+        }
+    }
+}
