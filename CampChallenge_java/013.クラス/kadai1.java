b47dd4ecb2599dc2b310e169dd0dd0c1215a8747
XTEND\XTEND_495 <s1200164@gmail.com>
2017-05-29 11:55:45 +0900

    クラスの利用in java クラスの作成の課題です

---
+++ CampChallenge_java/013.クラス/kadai1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author XTEND-495
+ */
+public class kadai1 {
+    public int i=0;
+    public int j=0;
+    public void setkadai1(int a,int b){
+        this.i=a;
+        this.j=b;
+    }
+    public void printkadai1(){
+        System.out.print(i+"<br>");
+        System.out.print(j);
+    }  
+}
