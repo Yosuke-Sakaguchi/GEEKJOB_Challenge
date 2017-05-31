c139a579e4548c9f258b535129e57b82deab640b
XTEND\XTEND_495 <s1200164@gmail.com>
2017-05-31 11:31:33 +0900

    文字列編　文字の入れ替えの課題です

---
+++ CampChallenge_java/015.標準クラス/kadai7.java
@@ -0,0 +1,9 @@
+public class kadai3 {
+    public static void main(String args[]){
+        String s="きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        String a=s.replace("I", "い");
+        String b=a.replace("U", "う");
+        System.out.println(b);        
+    }
+    
+}
