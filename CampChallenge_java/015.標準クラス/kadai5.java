002cebfc090bbc71af4c27f0653efd828d4174b4
XTEND\XTEND_495 <s1200164@gmail.com>
2017-05-31 11:30:41 +0900

    文字列編　バイト数と文字数の取得の課題です

---
+++ CampChallenge_java/015.標準クラス/kadai5.java
@@ -0,0 +1,8 @@
+public class kadai1 {
+    public static void main(String args[]){
+        String name="坂口耀介";
+        System.out.println("名前のバイト数は"+name.getBytes().length+"です");
+        System.out.println("名前の文字数は"+name.length()+"です");
+    }
+    
+}
