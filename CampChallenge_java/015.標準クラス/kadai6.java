17082cfb8862a17363adeec0b6c069bc4457872e
XTEND\XTEND_495 <s1200164@gmail.com>
2017-05-31 11:31:10 +0900

    文字列編　文字数の取得の課題です

---
+++ CampChallenge_java/015.標準クラス/kadai6.java
@@ -0,0 +1,6 @@
+public class kadai2 {
+    public static void main(String args[]){
+        String mail="s1200164@gmail.com";
+        System.out.println(mail.substring(mail.indexOf("@")));
+    }    
+}
